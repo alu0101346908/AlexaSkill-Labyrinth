d3d7fcc32e10c5f73ef24eed7f6235d9a4954bde
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-05 19:11:09 +0000

    updated lambda/world.js.

--- lambda/world.js
+++ lambda/world.js
@@ -1,8 +1,15 @@
+function World(i,j) {
+    let Cells = [];
+    for (var n = 0; n < i; n++) {
+        Cells.push([]);
+        for (var m = 0; m < j; m++) {
+            Cells[n].push(['0']);
+        }
+    }
+    this.cells = Cells;
+}
+ 
+// now we export the class, so other modules can create Cat objects
 module.exports = {
-    
-    
-    
-    
-    
-    
-};
+    World: World
+}
