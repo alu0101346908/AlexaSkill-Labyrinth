78ca8d298a6b87673e8e2431d29c979cbc55c6a5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-07 13:01:02 +0000

    updated lambda/world.js.

--- lambda/world.js
+++ lambda/world.js
@@ -1,28 +1,26 @@
 
-class World() {
-    constructor(i,j,size){
-        let Cells = [];
-        for (let n = 0; n < i; n++) {
-            Cells.push([]);
-            for (let m = 0; m < j; m++) {
-                Cells[n].push(['0']);
-            }
+function World(i,j,size) {
+    let Cells = [];
+    for (let n = 0; n < i; n++) {
+        Cells.push([]);
+        for (let m = 0; m < j; m++) {
+            Cells[n].push(['0']);
         }
-            let obstacles_percentage = 0.30;
-            let obstacles_number = size * obstacles_percentage;
-            obstacles_number = toString(obstacles_number);
-            obstacles_number = parseInt(obstacles_number);
-            let obstacle_counter = 0;
-        while (obstacle_counter < obstacles_number){
-            let row = Math.random(0,3);
-            let col = Math.random(0,3);
-            if ((Cells[row][col] != 'J') && (Cells[row][col] != 'X') && (Cells[row][col] != 'O')){
-                Cells[row][col] = 'X';
-                obstacle_counter++;
-            }
-        }
-        this.cells = Cells;
     }
+        let obstacles_percentage = 0.30;
+        let obstacles_number = size * obstacles_percentage;
+        obstacles_number = toString(obstacles_number);
+        obstacles_number = parseInt(obstacles_number);
+        let obstacle_counter = 0;
+    while (obstacle_counter < obstacles_number){
+        let row = Math.random(0,3);
+        let col = Math.random(0,3);
+        if ((Cells[row][col] !== 'J') && (Cells[row][col] !== 'X') && (Cells[row][col] !== 'O')){
+            Cells[row][col] = 'X';
+            obstacle_counter++;
+        }
+    }
+    this.cells = Cells;
     
 }
  
