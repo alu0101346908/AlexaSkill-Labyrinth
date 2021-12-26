81eed8312cc1e041fd021eed8d4cdb6d975d79ef
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-26 21:20:17 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -281,6 +281,7 @@ const PickObjectIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PickObjectIntent';
     },
     handle(handlerInput) {
+        //quitar el valor a recibir, solo recojo objeto y ya se maneja el tipo de objeto en la propia funcion
         const AnswerValue = handlerInput.requestEnvelope.request.intent.slots.Query.value;
         if (CurrentWorld == null){
             let speakOutput = "No existe laberinto, crea uno antes de empezar diciendo crea mundo pequeño, mediano o grande";
